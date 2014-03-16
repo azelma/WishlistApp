6c9dfe371f8fd37cdb884fe4368a63b7b82a3713
azelma <azelma@users.noreply.github.com>
2014-03-15 19:34:22 -0500

    Update membership.rb

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,6 @@
 class Membership < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :wishlist
+	validates_inclusion_of :contributor, :in =>[true, false]
 	validates_uniqueness_of :user_id, :scope => :wishlist_id
 end
