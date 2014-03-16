f554b297d819ba437a4b408489ce7b802be26f19
Valerie Woolard <valerie.woolard@gmail.com>
2014-03-16 09:34:00 -0500

    Added controller filter

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,4 +1,6 @@
 class WishlistsController < ApplicationController
+	before_action :authorized_users 
+	skip_before_action :authorized_users, only: [:new, :create, :index, :show]
 
 	def index
 		this_user_id = session[:user_id]
@@ -15,6 +17,7 @@ class WishlistsController < ApplicationController
 	def show
 		this_wishlist_id = params[:id]
 		this_user_id = session[:user_id]
+		# Doesn't use authorized user filter since more users can view than can perform other actions
 		authorized_users = Membership.where(:wishlist_id => this_wishlist_id).pluck(:user_id)
 		if authorized_users.include?(this_user_id)
 			membership_info = Membership.find_by(:wishlist_id => this_wishlist_id, :user_id => this_user_id)
@@ -51,92 +54,76 @@ class WishlistsController < ApplicationController
 			flash[:warning] = ''
       		for error in w.errors.full_messages
         		flash[:warning] += error + '<br>'
-     		 end
+     		end
 			redirect_to new_wishlist_url
 		end
 	end	
 
 	def edit
 		this_wishlist_id = params[:id]
-		authorized_users = Membership.where(:wishlist_id => this_wishlist_id).where(:contributor=>true).pluck(:user_id)
-		if authorized_users.include?(session[:user_id])
-			@wishlist = Wishlist.find_by(:id => this_wishlist_id)
-		else
-			flash[:alert] = "You don't have permission to edit that list"
-			redirect_to wishlists_url
-		end
+		@wishlist = Wishlist.find_by(:id => this_wishlist_id)
 	end
 
 	def update
 		this_wishlist_id = params[:id]
-		authorized_users = Membership.where(:wishlist_id => this_wishlist_id).where(:contributor=>true).pluck(:user_id)
-		if authorized_users.include?(session[:user_id])
-			w = Wishlist.find_by(:id => this_wishlist_id)
-			w.name = params[:name]
-			w.description = params[:description]
-			if w.save
-				redirect_to wishlist_path(this_wishlist_id)
-			else
-				flash[:warning] = ''
-      			for error in w.errors.full_messages
-        			flash[:warning] += error + '<br>'
-      			end
-				redirect_to edit_wishlist_path(this_wishlist_id)
-			end
+		w = Wishlist.find_by(:id => this_wishlist_id)
+		w.name = params[:name]
+		w.description = params[:description]
+		if w.save
+			redirect_to wishlist_path(this_wishlist_id)
 		else
-			flash[:alert] = "You don't have permission to edit that list"
-			redirect_to wishlists_url
+			flash[:warning] = ''
+      		for error in w.errors.full_messages
+        		flash[:warning] += error + '<br>'
+      		end
+			redirect_to edit_wishlist_path(this_wishlist_id)
 		end
 	end
 
 	def destroy
 		this_wishlist_id = params[:id]
-		authorized_users = Membership.where(:wishlist_id => this_wishlist_id).where(:contributor=>true).pluck(:user_id)
-		if authorized_users.include?(session[:user_id])
-			w = Wishlist.find_by(:id => this_wishlist_id)
-    		w.destroy
-	  		redirect_to wishlists_url
-	  	else
-	  		flash[:alert] = "You don't have permission to delete that list"
-	  		redirect_to wishlists_url
-	  	end
+		w = Wishlist.find_by(:id => this_wishlist_id)
+    	w.destroy
+	  	redirect_to wishlists_url
 	end
 
 	def additem
 		this_wishlist_id = params[:wishlist_id]
-		authorized_users = Membership.where(:wishlist_id => this_wishlist_id).where(:contributor=>true).pluck(:user_id)
-		if authorized_users.include?(session[:user_id])
-			this_item_id = params[:item_id]
-			w = Wishlist.find_by(:id => this_wishlist_id)
-			i = Item.find_by(:id => this_item_id)
-			if w and i
-				w.items << i
-				redirect_to wishlist_path(this_wishlist_id)
-			else
-				flash[:warning] = 'There was an error adding that item'
-				if w.nil?
-					redirect_to wishlists_url
-				else
-					redirect_to wishlist_path(this_wishlist_id)
-				end
-			end
+		this_item_id = params[:item_id]
+		w = Wishlist.find_by(:id => this_wishlist_id)
+		i = Item.find_by(:id => this_item_id)
+		if w and i
+			w.items << i
+			redirect_to wishlist_path(this_wishlist_id)
 		else
-			flash[:alert] = "You don't have permission to add items to that list"
-			redirect_to wishlists_url
+			flash[:warning] = 'There was an error adding that item'
+			if w.nil?
+				redirect_to wishlists_url
+			else
+				redirect_to wishlist_path(this_wishlist_id)
+			end
 		end
 	end
 
 	def removeitem
 		this_wishlist_id = params[:wishlist_id]
-		authorized_users = Membership.where(:wishlist_id => this_wishlist_id).where(:contributor=>true).pluck(:user_id)
-		if authorized_users.include?(session[:user_id])
-			this_item_id = params[:item_id]
-			w = Wishlist.find_by(:id => this_wishlist_id)
-			i = Item.find_by(:id => this_item_id)
-			w.items.delete(i)
-			redirect_to wishlist_path(this_wishlist_id)
+		this_item_id = params[:item_id]
+		w = Wishlist.find_by(:id => this_wishlist_id)
+		i = Item.find_by(:id => this_item_id)
+		w.items.delete(i)
+		redirect_to wishlist_path(this_wishlist_id)
+	end
+
+	private
+	def authorized_users
+		if params[:wishlist_id]
+			this_wishlist_id = params[:wishlist_id]
 		else
-			flash[:alert] = "You don't have permission to delete items from that list"
+			this_wishlist_id = params[:id]
+		end
+		authorized_users = Membership.where(:wishlist_id => this_wishlist_id).where(:contributor=>true).pluck(:user_id)
+		unless authorized_users.include?(session[:user_id])
+			flash[:alert] = "You don't have permission to do that"
 			redirect_to wishlists_url
 		end
 	end
